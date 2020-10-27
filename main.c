2a611f1edf5fdb0318be79cf2debe4e45f393de1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-27 10:12:03 +0100

    Used average function

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include "circleArea.h"
 #include "writeRandom.h"
+#include "average.h"
 
 int main() {
     circularAreaMain();
     writeRandom();
+
+    const int ARR_SIZE = 5;
+    float arr[ARR_SIZE] = {0.5, 9.3, 2.0, 3.045, 4.0};
+    double avg = average(arr, ARR_SIZE);
+
+    printf("The average is: %f", avg);
 
     return 0;
 }
