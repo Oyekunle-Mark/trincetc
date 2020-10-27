e107c3c64d5c99b2733d7125a551c8a68d68da7b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-27 10:07:38 +0100

    The average function

--- average.h
+++ average.h
@@ -5,4 +5,18 @@
 #ifndef TRINCETC_AVERAGE_H
 #define TRINCETC_AVERAGE_H
 
+double average(const float *array, int length) {
+    double sum = 0.0;
+    const float *end = array + length;
+
+    if (length <= 0)
+        return 0.0;
+
+    for (const float *itr = array; itr < end; ++itr) {
+        sum += *itr;
+    }
+
+    return sum / length;
+}
+
 #endif //TRINCETC_AVERAGE_H
