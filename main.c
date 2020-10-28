93c9d9294576b6273b3b52e83b2a96028a023d24
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-28 17:23:18 +0100

    The read and find average function

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "circleArea.h"
 #include "writeRandom.h"
 #include "average.h"
+#include "readNumberAndFindAverage.h"
 
 int main() {
     circularAreaMain();
@@ -10,6 +11,8 @@ int main() {
     double avg = average(arr, sizeof(arr) / sizeof(*arr));
 
     printf("The average is: %f", avg);
+
+    readAndFindAverage();
 
     return 0;
 }
--- readNumberAndFindAverage.c
+++ readNumberAndFindAverage.c
@@ -1,5 +1,28 @@
 //
 // Created by Oyekunle Oloyede on 28/10/2020.
 //
-
+#include <stdio.h>
 #include "readNumberAndFindAverage.h"
+
+/**
+ * Read in numbers from the keyboard and find the average
+ */
+void readAndFindAverage() {
+    double sum = 0.0;
+    double input = 0.0;
+    int count = 0;
+
+    printf("\t--- Calculate Averages ---\n");
+    printf("\nEnter some numbers:\n"
+           "(Type a letter to end your input)\n");
+
+    while (scanf("%lf", &input) == 1) {
+        sum += input;
+        ++count;
+    }
+
+    if (count == 0)
+        printf("No input data!\n");
+    else
+        printf("The average of your numbers is %.2f\n", sum / count);
+}
