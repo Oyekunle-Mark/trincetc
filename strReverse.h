2858bdf02b86197df81770f03ef595f9717bccbe
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-28 17:29:05 +0100

    REverses a string

--- strReverse.h
+++ strReverse.h
@@ -5,4 +5,14 @@
 #ifndef TRINCETC_STRREVERSE_H
 #define TRINCETC_STRREVERSE_H
 
+#include <string.h>
+
+void strReverse(char *str) {
+    for (size_t i = 0, j = strlen(str) - 1; i < j; ++i, --j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
 #endif //TRINCETC_STRREVERSE_H
