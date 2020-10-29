336558447afe5cd3f216983420babc764ad56918
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-29 21:25:05 +0100

    First pass

--- strCat.h
+++ strCat.h
@@ -5,4 +5,15 @@
 #ifndef TRINCETC_STRCAT_H
 #define TRINCETC_STRCAT_H
 
+char *strCat(char *s1, const char *s2) {
+    char *s1Ptr = s1;
+
+    while (*s1 != '\0')
+        ++s1;
+
+    while ((*s1++ = *s2++) != '\0');
+
+    return s1Ptr;
+}
+
 #endif //TRINCETC_STRCAT_H
