5e53a77c355673e1381479f3dabc728e4e0d4d98
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-31 20:18:13 +0100

    The header file

--- sortLines.h
+++ sortLines.h
@@ -5,4 +5,11 @@
 #ifndef TRINCETC_SORTLINES_H
 #define TRINCETC_SORTLINES_H
 
+#define N_LINES_MAX 1000
+#define LEN_MAX 512
+
+char *getLine(void);
+
+int strCompare(const void *, const void *);
+
 #endif //TRINCETC_SORTLINES_H
