2966b673c23d4c2169acdd04a01e9de20d762e40
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-10-31 20:40:33 +0100

    Performs basic arithmetic on two integers

---
+++ performMath.h
@@ -0,0 +1,24 @@
+//
+// Created by Oyekunle Oloyede on 31/10/2020.
+//
+
+#ifndef TRINCETC_PERFORMMATH_H
+#define TRINCETC_PERFORMMATH_H
+
+double add(double x, double y) {
+    return x + y;
+}
+
+double sub(double x, double y) {
+    return x - y;
+}
+
+double mult(double x, double y) {
+    return x * y;
+}
+
+double div(double x, double y) {
+    return x / y;
+}
+
+#endif //TRINCETC_PERFORMMATH_H
