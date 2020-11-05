bea6273ca2734c7c88d8dbad7860014d52fd0171
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-05 17:32:31 +0100

    isReadWritable progam

--- isReadWritable.h
+++ isReadWritable.h
@@ -1,11 +1,21 @@
 //
 // Created by Oyekunle Oloyede on 05/11/2020.
 //
+#include <stdio.h>
+#include <stdbool.h>
+
 #ifndef TRINCETC_ISREADWRITABLE_H
 #define TRINCETC_ISREADWRITABLE_H
 
 _Bool isReadWritable(const char *filename) {
+    FILE *fp = fopen(filename, "r+");
 
+    if (fp != NULL) {
+        fclose(fp);
+        return true;
+    }
+
+    return false;
 }
 
 #endif //TRINCETC_ISREADWRITABLE_H
