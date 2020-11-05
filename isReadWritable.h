8c8753aa64a1578ade0ce64592c634c64e40be4d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-11-05 17:30:26 +0100

    Program to check if a file is read and writable

---
+++ isReadWritable.h
@@ -0,0 +1,11 @@
+//
+// Created by Oyekunle Oloyede on 05/11/2020.
+//
+#ifndef TRINCETC_ISREADWRITABLE_H
+#define TRINCETC_ISREADWRITABLE_H
+
+_Bool isReadWritable(const char *filename) {
+
+}
+
+#endif //TRINCETC_ISREADWRITABLE_H
